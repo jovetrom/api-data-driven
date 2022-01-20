93c373840d1f9e453d925a11a1f3c2a5cd6b0125
Jovelino José Alves da Silva <jovelino.silva@pli-petronas.com>
2022-01-20 17:46:56 -0300

    Alteracao de database in memory para sql server Azure

--- Startup.cs
+++ Startup.cs
@@ -55,8 +55,8 @@ namespace Shop
                 };
             });
 
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
-            //services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+            //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
 
             services.AddSwaggerGen(c =>
             {
