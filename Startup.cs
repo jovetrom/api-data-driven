eb1e0417e5bb092b43eb61ee184c7e838ab75f30
Jovelino José Alves da Silva <jovelino.silva@pli-petronas.com>
2022-01-20 17:51:39 -0300

    Startup permitindo visualização do erro em prd

--- Startup.cs
+++ Startup.cs
@@ -67,10 +67,8 @@ namespace Shop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
-            }
 
             app.UseHttpsRedirection();
 
